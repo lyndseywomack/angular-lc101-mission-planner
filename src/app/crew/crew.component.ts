f8bb3e178b4fc91ed77927a07d83c3d7fca6623f
Lyndsey Womack <lyndseywomack@lyndseys-air.lan>
2021-08-17 19:49:49 -0500

    completed studio; no bonus

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -12,9 +12,29 @@ export class CrewComponent implements OnInit {
     {name: "Mae Jemison", firstMission: false},
     {name: "Ellen Ochoa", firstMission: true}
   ];
+  memberBeingEdited: object = null;
+
 
   constructor() { }
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
 
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+ save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
+  }
+  
+  
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+  
   ngOnInit() {
   }
 
